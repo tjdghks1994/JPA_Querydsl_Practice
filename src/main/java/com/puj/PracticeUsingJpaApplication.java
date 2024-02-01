ebdca77a7e0ca619ec0572f8601017da0061627e
SungHwan <tjdghks130907@gmail.com>
2024-02-01 21:54:37 +0900

    git init project

---
+++ src/main/java/com/puj/PracticeUsingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.puj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeUsingJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeUsingJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/puj/PracticeUsingJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.puj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeUsingJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
