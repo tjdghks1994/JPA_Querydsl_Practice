e56e240fa592084f605f3dd564cc981162eae198
SungHwan <tjdghks130907@gmail.com>
2024-02-18 17:06:21 +0900

    [feature-basicrepo] 게시글 타입 enum 클래스 수정
    - 필드에 final 키워드 추가

--- src/main/java/com/puj/domain/board/BoardType.java
+++ src/main/java/com/puj/domain/board/BoardType.java
@@ -3,7 +3,7 @@ package com.puj.domain.board;
 public enum BoardType {
     NOTICE("공지사항"), NORMAL("일반게시글"), QnA("QnA");
 
-    private String typeValue;
+    private final String typeValue;
 
     BoardType(String typeValue) {
         this.typeValue = typeValue;
