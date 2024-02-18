cb68bd928d68255144ecd4bba413065557bb48ae
SungHwan <tjdghks130907@gmail.com>
2024-02-18 17:00:41 +0900

    [feature-basicrepo] 회원 엔티티 수정
    - 컬럼 디폴트 값 명시를 위한 어노테이션 추가 (해당 어노테이션은 실제 디폴트 값을 세팅해주는 어노테이션은 아님)
    - @PrePersist 어노테이션을 활용하여 persist() 호출 전에 기본 값 세팅을 위한 로직 추가
    - Builder 에서 pwd 필드를 빼먹어서 추가

--- src/main/java/com/puj/domain/member/Member.java
+++ src/main/java/com/puj/domain/member/Member.java
@@ -3,6 +3,7 @@ package com.puj.domain.member;
 import com.puj.domain.base.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 @Entity
 @Getter
@@ -26,6 +27,7 @@ public class Member extends BaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "member_role")
+    @ColumnDefault(value = "NORMAL")
     private MemberRole role;
 
     @Column(name = "member_key")
@@ -33,11 +35,19 @@ public class Member extends BaseTimeEntity {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "member_status")
+    @ColumnDefault(value = "ENABLE")
     private MemberStatus status;
 
+    @PrePersist
+    protected void fieldDefaultValueSetting() {
+        this.role = this.role == null ? MemberRole.NORMAL : this.role;
+        this.status = this.status == null ? MemberStatus.ENABLE : this.status;
+    }
+
     @Builder
-    private Member(String email, String nickname, MemberRole role, String oAuthKey, MemberStatus status) {
+    private Member(String email, String pwd, String nickname, MemberRole role, String oAuthKey, MemberStatus status) {
         this.email = email;
+        this.pwd = pwd;
         this.nickname = nickname;
         this.role = role;
         this.oAuthKey = oAuthKey;
