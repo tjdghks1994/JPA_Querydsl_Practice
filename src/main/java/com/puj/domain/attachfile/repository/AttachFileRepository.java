e32029ac0f9ad88d2f3e34e11f689482d589a40f
SungHwan <tjdghks130907@gmail.com>
2024-02-24 22:41:58 +0900

    [feature-basicservice] 첨부파일 목록 조회
    - 게시글에 존재하는 첨부파일 목록을 조회하는 기능 구현
    - 메서드 이름으로 쿼리 생성할 시 left join 쿼리가 발생하여, @Query 어노테이션을 활용해 직접 JPQL 쿼리 작성

--- src/main/java/com/puj/domain/attachfile/repository/AttachFileRepository.java
+++ src/main/java/com/puj/domain/attachfile/repository/AttachFileRepository.java
@@ -2,7 +2,14 @@ package com.puj.domain.attachfile.repository;
 
 import com.puj.domain.attachfile.AttachFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 // Spring Data JPA 사용
 public interface AttachFileRepository extends JpaRepository<AttachFile, Long> {
+
+    @Query("select a from AttachFile a join a.board b where b.id = :boardId")
+    public List<AttachFile> findAttachFileByBoardId(@Param("boardId") Long boardId);
 }
