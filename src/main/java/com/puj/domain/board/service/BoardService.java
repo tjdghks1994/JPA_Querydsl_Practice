55255159bf45e4c5d1c901c3e1b6b7c8ffc604ba
SungHwan <tjdghks130907@gmail.com>
2024-02-26 22:56:58 +0900

    [feature-basicservice] 게시글 단건 조회
    - 게시글 단건을 조회하는 기능 구현
    - 게시글과 연관관계가 있는 첨부파일, 댓글도 조회하여 응답 DTO 클래스로 반환

--- src/main/java/com/puj/domain/board/service/BoardService.java
+++ src/main/java/com/puj/domain/board/service/BoardService.java
@@ -1,8 +1,16 @@
 package com.puj.domain.board.service;
 
+import com.puj.domain.attachfile.AttachFile;
+import com.puj.domain.attachfile.repository.AttachFileRepository;
+import com.puj.domain.attachfile.repository.dto.SearchAttachResp;
 import com.puj.domain.board.Board;
+import com.puj.domain.board.exception.InvalidBoardException;
 import com.puj.domain.board.repository.BoardRepository;
 import com.puj.domain.board.service.dto.CreateBoardReq;
+import com.puj.domain.board.service.dto.SearchBoardResp;
+import com.puj.domain.comment.Comment;
+import com.puj.domain.comment.repository.CommentRepository;
+import com.puj.domain.comment.repository.dto.SearchCommentResp;
 import com.puj.domain.member.Member;
 import com.puj.domain.member.exception.InvalidMemberException;
 import com.puj.domain.member.repository.MemberRepository;
@@ -11,6 +19,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Service
 @Transactional
@@ -18,7 +29,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class BoardService {
     private final BoardRepository boardRepository;
     private final MemberRepository memberRepository;
+    private final AttachFileRepository attachFileRepository;
+    private final CommentRepository commentRepository;
 
+    // 게시글 생성
     public Long createBoard(CreateBoardReq req) {
         // 작성자 조회
         Member findMember = memberRepository.findByEmail(req.getWriter())
@@ -34,5 +48,46 @@ public class BoardService {
         Board saveBoard = boardRepository.save(boardEntity);
 
         return saveBoard.getId();
+    }
+
+    // 게시글 단건 조회
+    public SearchBoardResp searchBoard(Long boardId) {
+        // 게시글 조회
+        Board findBoard = boardRepository.findBoardByIdWithMember(boardId)
+                .orElseThrow(() -> new InvalidBoardException("존재하지 않는 게시글 입니다."));
+
+        // 해당 게시글의 첨부파일 목록 조회
+        List<SearchAttachResp> attachFileList = attachFileRepository.findAttachFileByBoardId(boardId)
+                .stream()
+                .map((a) -> SearchAttachResp.builder()
+                                            .attachId(a.getId())
+                                            .originFilename(a.getOriginFilename())
+                                            .attachExtension(a.getAttachExtension())
+                                            .imageYN(a.getImageYN())
+                                            .build())
+                .collect(Collectors.toList());
+
+        // 해당 게시글의 댓글 목록 조회
+        List<SearchCommentResp> commentList = commentRepository.findCommentByBoardId(boardId)
+                .stream()
+                .map((c) -> SearchCommentResp.builder()
+                                            .commentId(c.getId())
+                                            .commentContent(c.getContent())
+                                            .createdAt(c.getCreatedAt())
+                                            .parentCommentId(c.getParentComment().getId())
+                                            .build()
+                ).collect(Collectors.toList());
+
+        SearchBoardResp resp = SearchBoardResp.builder()
+                .boardId(findBoard.getId())
+                .boardTitle(findBoard.getBoardTitle())
+                .boardContent(findBoard.getBoardContent())
+                .viewCnt(findBoard.getViewCnt())
+                .writer(findBoard.getMember().getNickname())
+                .attachList(attachFileList)
+                .commentList(commentList)
+                .build();
+
+        return resp;
     }
 }
