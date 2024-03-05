5e218e699a54cd22691722d089129f93d9dca26b
SungHwan <tjdghks130907@gmail.com>
2024-03-05 23:14:22 +0900

    [feature-basicservice] 첨부파일 기능 테스트
    - 게시글 첨부파일 목록 조회 기능 테스트
    - 게시글 첨부파일 단건 조회 기능 테스트
    - 첨부파일 벌크 update 기능 테스트

--- src/test/java/com/puj/domain/attachfile/repository/AttachFileRepositoryTest.java
+++ src/test/java/com/puj/domain/attachfile/repository/AttachFileRepositoryTest.java
@@ -6,14 +6,20 @@ import com.puj.domain.board.BoardType;
 import com.puj.domain.board.repository.BoardRepository;
 import com.puj.domain.member.Member;
 import com.puj.domain.member.repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.*;
@@ -33,14 +39,23 @@ class AttachFileRepositoryTest {
     @Autowired
     AttachFileRepository attachFileRepository;
 
-    @Test
-    void findAttachFileByBoardId() {
-        Member member = Member.builder()
+    @PersistenceContext
+    EntityManager em;
+
+    private Member member;
+    private Board board;
+    private AttachFile attachFile1;
+    private AttachFile attachFile2;
+
+    @BeforeEach
+    @DisplayName("매 테스트마다 중복되는 엔티티들을 미리 세팅하기 위함")
+    void initEntity() {
+        member = Member.builder()
                 .email("test@naver.com")
                 .pwd("askdfeizxoci23lrn13lksl")
                 .nickname("하디")
                 .build();
-        Board newBoard = Board.builder()
+        board = Board.builder()
                 .boardTitle("게시글 제목")
                 .boardContent("게시글 내용은 무수히 많은 문자열이 작성될 수 있습니다.")
                 .boardType(BoardType.NORMAL)
@@ -48,32 +63,66 @@ class AttachFileRepositoryTest {
                 .deleteYN("Y")
                 .parentBoard(null)
                 .build();
-        AttachFile attachFile = AttachFile.builder()
+        attachFile1 = AttachFile.builder()
                 .attachPath("/home/attach/")
                 .saveFilename(UUID.randomUUID().toString())
                 .originFilename("To-do List")
                 .attachExtension(".txt")
                 .imageYN("N")
-                .board(newBoard)
+                .board(board)
                 .build();
-        AttachFile attachFile2 = AttachFile.builder()
+        attachFile2 = AttachFile.builder()
                 .attachPath("/home/attach/")
                 .saveFilename(UUID.randomUUID().toString())
                 .originFilename("To-do List2")
                 .attachExtension(".txt")
                 .imageYN("N")
-                .board(newBoard)
+                .board(board)
                 .build();
 
-        Member saveMember = memberRepository.save(member);
-        Board saveBoard = boardRepository.save(newBoard);
-        AttachFile saveFile1 = attachFileRepository.save(attachFile);
-        AttachFile saveFile2 = attachFileRepository.save(attachFile2);
+        memberRepository.save(member);
+        boardRepository.save(board);
+        attachFileRepository.save(attachFile1);
+        attachFileRepository.save(attachFile2);
+    }
 
-        List<AttachFile> attachFileList = attachFileRepository.findAttachFileByBoardId(saveBoard.getId());
-        List<AttachFile> attachFileList2 = attachFileRepository.findAttachFileByBoardId(0L);
+    @Test
+    @DisplayName("게시글 첨부파일 목록 조회 테스트")
+    void findAttachFileByBoardId() {
+        List<AttachFile> findAttachList1 = attachFileRepository.findAttachFileByBoardId(board.getId());
+        List<AttachFile> findAttachList2 = attachFileRepository.findAttachFileByBoardId(0L);
 
-        assertThat(attachFileList.size()).isEqualTo(2);
-        assertThat(attachFileList2.size()).isEqualTo(0);
+        assertThat(findAttachList1.size()).isEqualTo(2);
+        assertThat(findAttachList2.size()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("게시글 첨부파일 단건 조회 테스트")
+    void findAttachFileByIdAndBoardId() {
+        Optional<AttachFile> findAttachFile1 = attachFileRepository.findAttachFileByIdAndBoardId(attachFile1.getId(), board.getId());
+        Optional<AttachFile> findAttachFile2 = attachFileRepository.findAttachFileByIdAndBoardId(attachFile2.getId(), board.getId());
+
+        assertThat(findAttachFile1.get()).isEqualTo(attachFile1);
+        assertThat(findAttachFile2.get()).isEqualTo(attachFile2);
+    }
+
+    @Test
+    @DisplayName("첨부파일 벌크 update 삭제 테스트 - deleteYN 값을 Y로 변경")
+    void bulkAttachFileDelete() {
+        List<Long> deleteAttachFileIds = Arrays.asList(attachFile1.getId(), attachFile2.getId());
+
+        int resultCount = attachFileRepository.bulkAttachFileDelete(board.getId(), deleteAttachFileIds);
+        // 벌크성 쿼리 실행 후 관련된 엔티티를 조회해야할 경우, 영속성 컨텍스트에 벌크성 쿼리 이전 상태의 값이 남아 있으므로
+        // 관련된 엔티티를 조회할 경우에는 영속성 컨텍스트를 초기화!
+        em.clear();
+        em.close();
+
+        Optional<AttachFile> findAttachFile1 = attachFileRepository.findAttachFileByIdAndBoardId(attachFile1.getId(), board.getId());
+        Optional<AttachFile> findAttachFile2 = attachFileRepository.findAttachFileByIdAndBoardId(attachFile2.getId(), board.getId());
+
+        assertThat(resultCount).isEqualTo(2);
+        assertThat(findAttachFile1.get().getDeleteYN()).isEqualTo("Y");
+        assertThat(findAttachFile2.get().getDeleteYN()).isEqualTo("Y");
+
     }
 }
