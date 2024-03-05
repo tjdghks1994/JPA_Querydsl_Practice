280450955e10b9b6c04ad64f9b82a86d00be3800
SungHwan <tjdghks130907@gmail.com>
2024-03-05 23:13:01 +0900

    [feature-basicservice] 첨부파일 쿼리메서드 추가
    - 파라미터로 넘어온 첨부파일 ID와 게시글 ID 값을 가진 첨부파일 엔티티가 있는지 조회하는 메서드 추가
    - 삭제하려는 첨부파일 목록들의 deleteYN 값을 변경시키기 위한 벌크성 수정 쿼리 메서드 추가

--- src/main/java/com/puj/domain/attachfile/repository/AttachFileRepository.java
+++ src/main/java/com/puj/domain/attachfile/repository/AttachFileRepository.java
@@ -2,14 +2,23 @@ package com.puj.domain.attachfile.repository;
 
 import com.puj.domain.attachfile.AttachFile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 // Spring Data JPA 사용
 public interface AttachFileRepository extends JpaRepository<AttachFile, Long> {
 
-    @Query("select a from AttachFile a join a.board b where b.id = :boardId")
-    public List<AttachFile> findAttachFileByBoardId(@Param("boardId") Long boardId);
+    @Query("select a from AttachFile a join a.board b where b.id = :boardId and a.deleteYN = 'N'")
+    List<AttachFile> findAttachFileByBoardId(@Param("boardId") Long boardId);
+
+//    @Query("select a from AttachFile a join a.board b where a.id = :attachId and b.id = :boardId")
+    Optional<AttachFile> findAttachFileByIdAndBoardId(@Param("attachId") Long attachId, @Param("boardId") Long boardId);
+
+    @Modifying
+    @Query("update AttachFile a set a.deleteYN = 'Y' where a.board.id = :boardId and a.id in :attachIds")
+    int bulkAttachFileDelete(@Param("boardId") Long boardId, @Param("attachIds") List<Long> attachIds);
 }
