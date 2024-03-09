2290e90b3a928150b40df7d4751393fafb7f17e2
SungHwan <tjdghks130907@gmail.com>
2024-03-09 22:07:28 +0900

    [feature-basicservice] 게시글 삭제 요청 DTO 클래스
    - 게시글 삭제 요청 시, 필요한 정보를 담고 있는 DTO 클래스 추가

---
+++ src/main/java/com/puj/domain/board/service/DeleteBoardReq.java
@@ -0,0 +1,16 @@
+package com.puj.domain.board.service;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class DeleteBoardReq {
+    private Long boardId;
+    private String writer;
+
+    @Builder
+    private DeleteBoardReq(Long boardId, String writer) {
+        this.boardId = boardId;
+        this.writer = writer;
+    }
+}
