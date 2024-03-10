afac861eb919f49903fe9ac8c97828d8437ddf48
SungHwan <tjdghks130907@gmail.com>
2024-03-10 17:21:12 +0900

    [feature-basicservice] 필수 회원 예외 클래스 생성
    - 게시글 저장시 회원이 필수인데, null인 경우 예외를 발생시키기 위한 커스텀 예외 클래스 작성

---
+++ src/main/java/com/puj/domain/member/exception/RequiredMemberException.java
@@ -0,0 +1,7 @@
+package com.puj.domain.member.exception;
+
+public class RequiredMemberException extends MemberException {
+    public RequiredMemberException(String message) {
+        super(message);
+    }
+}
