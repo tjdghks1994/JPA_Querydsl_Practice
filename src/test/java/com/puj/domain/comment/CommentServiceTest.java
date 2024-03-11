c6aa49c3c40b2de28fd7b3416fa2cdcce1026d96
SungHwan <tjdghks130907@gmail.com>
2024-03-11 22:43:48 +0900

    [test] 댓글 기능 테스트
    - 서비스 클래스 분리로 인해, 새롭게 작성한 댓글 서비스 클래스의 기능 테스트 진행
    - 현재 개발한 로직은 게시글의 댓글 목록을 조회하는 기능만 있어, 해당 기능 테스트만 진행

---
+++ src/test/java/com/puj/domain/comment/CommentServiceTest.java
@@ -0,0 +1,71 @@
+package com.puj.domain.comment;
+
+import com.puj.domain.board.Board;
+import com.puj.domain.board.BoardType;
+import com.puj.domain.comment.repository.dto.SearchCommentResp;
+import com.puj.domain.member.Member;
+import com.puj.domain.member.MemberRole;
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@Transactional
+@SpringBootTest
+class CommentServiceTest {
+
+    @Autowired
+    private CommentService service;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("게시글의 댓글 목록 조회 테스트")
+    void searchCommentListTest() {
+        Member member = Member.builder()
+                .email("hello@gmail.com")
+                .pwd(UUID.randomUUID().toString())
+                .role(MemberRole.NORMAL)
+                .nickname("hardy")
+                .oAuthKey(null)
+                .build();
+        Board board = Board.builder()
+                .boardTitle("test")
+                .boardContent("hello world!")
+                .boardType(BoardType.NORMAL)
+                .parentBoard(null)
+                .member(member)
+                .build();
+        Comment comment1 = Comment.builder()
+                .content("hihi")
+                .member(member)
+                .board(board)
+                .parentComment(null)
+                .build();
+        Comment comment2 = Comment.builder()
+                .content("JPA는 어려워~")
+                .member(member)
+                .board(board)
+                .parentComment(null)
+                .build();
+        em.persist(member);
+        em.persist(board);
+        em.persist(comment1);
+        em.persist(comment2);
+
+        List<SearchCommentResp> searchCommentResps = service.searchCommentList(board.getId());
+
+        Assertions.assertThat(searchCommentResps.size()).isEqualTo(2);
+    }
+}
